0b6daecce60f114ca200587e47b156360f1b0303
Carl Harroch <carl@novoda.com>
2011-03-26 15:23:55 +0000

    added some default values so we get something else then a forceclose

--- timescape_plugin/src/rewired/state/receiver/PluginReceiver.java
+++ timescape_plugin/src/rewired/state/receiver/PluginReceiver.java
@@ -1,19 +1,47 @@
+
 package rewired.state.receiver;
 
 import novoda.lib.se.idores.provider.PluginContentValues;
 import novoda.lib.se.idores.provider.SourceContentValues;
 import novoda.lib.se.idores.receiver.EventStreamReceiver;
+import rewired.state.R;
+import rewired.state.activity.Settings;
+
+import android.content.ComponentName;
 
 public class PluginReceiver extends EventStreamReceiver {
 
     @Override
     public PluginContentValues getRegisterValues() {
-        return null;
+        boolean isloggedin = true;
+        PluginContentValues values = new PluginContentValues(mContext);
+        values.setApiVersion(1);
+        values.setName(getContext().getString(R.string.app_name));
+        values.setPluginKey("testing");
+        values.setIcon(R.drawable.icon);
+        values.setStatusSupport(1);
+        values.setStatusMaxLength(256);
+        values.setState((isloggedin) ? 0 : 1);
+        values.setConfigurationStatus("test");
+        // values.setConfigurationStatus((isloggedin) ?
+        // mContext.getString(R.string.logged_in_as)
+        // + " " + PreferenceUtil.getUsername(getContext()) : mContext
+        // .getString(R.string.not_logged_in));
+
+        ComponentName activity = new ComponentName(mContext, Settings.class);
+        values.setConfigurationActivity(activity);
+        return values;
     }
 
     @Override
     public SourceContentValues[] getSources() {
-        return null;
+        SourceContentValues[] values = new SourceContentValues[1];
+        SourceContentValues.Builder builder = new SourceContentValues.Builder(mContext);
+        builder.withName(getContext().getString(R.string.app_name));
+        builder.withIcon(R.drawable.icon);
+        builder.isEnabled(true);
+        values[0] = builder.build();
+        return values;
     }
 
 }
